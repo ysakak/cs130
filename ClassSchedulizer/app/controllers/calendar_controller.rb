b111acce53ef8899b4f5b17495e964a93e85e37b
Yuuma <yuuma.sakakibara94@gmail.com>
2016-11-20 15:34:48 -0800

    now able to add dependent classes to calendar.

--- ClassSchedulizer/app/controllers/calendar_controller.rb
+++ ClassSchedulizer/app/controllers/calendar_controller.rb
@@ -2,16 +2,15 @@ class CalendarController < ApplicationController
   def index
     # for demo only, remove later.
     @class = IndependentClassData.where(["title = 'Introduction to Computer Science I'"])[0]
+    @dependent_class = DependentClassData.where(["class_id = 187093202"])[0]
     session[:chosen_classes] = []
     (session[:chosen_classes] ||= []) << @class
+    (session[:chosen_classes] ||= []) << @dependent_class
 
     chosen_class_json_array = []
-    
+
     for chosen_class in session[:chosen_classes]
       chosen_class_json_array.push(chosen_class.as_json)
-      for dependent_class in chosen_class.dependent_classes
-        chosen_class_json_array.push(dependent_class.as_json)
-      end
     end
 
     @chosen_classes = chosen_class_json_array.to_json().html_safe()
