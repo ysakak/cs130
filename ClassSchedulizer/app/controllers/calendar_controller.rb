23ea5dc91de65b6c26251320dbd2a9dd29382bb7
Yuuma <yuuma.sakakibara94@gmail.com>
2016-11-20 02:52:51 -0800

    adding calendar, now adds classes stored in session to calendar.

--- ClassSchedulizer/app/controllers/calendar_controller.rb
+++ ClassSchedulizer/app/controllers/calendar_controller.rb
@@ -1,4 +1,11 @@
 class CalendarController < ApplicationController
   def index
+    # for demo only, remove later.
+    @class = IndependentClassData.where(["title = 'Introduction to Computer Science I'"])[0]
+    session[:chosen_classes] = []
+    (session[:chosen_classes] ||= []) << @class.as_json()
+
+    @chosen_classes = session[:chosen_classes].to_json.html_safe
+
   end
 end
