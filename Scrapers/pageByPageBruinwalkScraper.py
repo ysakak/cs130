6f3bd20305ae88294a4935e9dc50e0441ae2cb51
Jayna Patel <jnaptl1@gmail.com>
2016-11-22 19:09:24 -0800

    bruinwalk scraper funtionality done
    finished the overall functionality of the Bruinwalk scraper, but now need to work on making the class names better outputted

--- Scrapers/pageByPageBruinwalkScraper.py
+++ Scrapers/pageByPageBruinwalkScraper.py
@@ -7,14 +7,15 @@ import re
 import string
 import sys
 import requests
+from lxml import html
 
 
-''' Stores the data for each class '''
-class ClassRatingData:
-	def _init_(self, class_name, website_title, url):
-		self.class_name = class_name
-		self.website_title = website_title
-		self.url = url
+#''' Stores the data for each class '''
+#class ClassRatingData:
+#	def _init_(self, class_name, website_title, url):
+#		self.class_name = class_name
+#		self.website_title = website_title
+#		self.url = url
 
 ''' Returns the page for a given url. Retries up to 10 times to retrieve the page. '''
 def get_page(url):
@@ -43,34 +44,51 @@ def get_class_name_and_rating(url):
 #	print html
 
 	classNameRegEx1 = '<div class="title circle main">(.+?)</div>'
-	classNameRegEx2 = '<div class="title circle">(.+?)</div>'
 	classNamePattern1 = re.compile(classNameRegEx1)
-	classNamePattern2 = re.compile(classNameRegEx2)
-
 	classNamesAllClasses = re.findall(classNamePattern1, html)
-	classNameFromClassPage = re.findall(classNamePattern2, html)
-	
-	classRatingRegEx1 = '<span class="rating"><b>(.+?)</b><i>Overall rating</i></span>'
-	classRatingPattern1 = re.compile(classRatingRegEx1)
 
-	classRatingAllClasses = re.findall(classRatingPattern1, html)
-	print classRatingAllClasses
-
-	j=0
 	i=0
+	j=0
 	while j<len(classNamesAllClasses):
 		print classNamesAllClasses[j]
-#		print classRatingAllClasses[i]
+		classRating = get_class_rating(page, i)
+		print classRating
 		j += 2
-#		i += 1
+		i += 1
+		if(i==10):
+			i=0
 
-get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=1")
+
+def get_class_rating(page, number):
+	tree = html.fromstring(page.content)
+	xPath = "/html/body/section/div/section/div[2]/div[2]/div[4]/div[" + switch_statement(number) + "]/div/div[1]/span/b//text()"
+	classRating = tree.xpath(xPath)
+	return classRating
+
+#get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=1")
 #get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=2")
 #get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=3")
 
+def switch_statement(number):
+	return {
+		0: '1',
+		1: '3',
+		2: '5',
+		3: '8',
+		4: '10',
+		5: '12',
+		6: '15',
+		7: '17',
+		8: '19',
+		9: '22',
+	}[number]
 
 
+def output_all_class_names():
+	i =1
+	while(i<=100):
+		url = "http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=" + str(i)
+		get_class_name_and_rating(url)
+		i += 1
 
-
-
-
+output_all_class_names()
