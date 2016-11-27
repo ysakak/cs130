322ead45c09c032262a4acec2e577ab53ee00187
Jayna Patel <jnaptl1@gmail.com>
2016-11-26 21:59:53 -0800

    Now writes to csv file
    writes class name and class rating to csv file. but only holds onto the last 11 items i add to the csv file...need help figuring out how to ADD to the file, instead of replace. [need to do: fix the class name output to not include dashes and amp (using default encoding did not work), add to the csv file instead of replace, include all classes instead of just the first 1000 listed on bruinwalk]

--- Scrapers/bruinwalk_scraper.py
+++ Scrapers/bruinwalk_scraper.py
@@ -9,13 +9,11 @@ import sys
 import requests
 from lxml import html
 
-
-#''' Stores the data for each class '''
-#class ClassRatingData:
-#	def _init_(self, class_name, website_title, url):
-#		self.class_name = class_name
-#		self.website_title = website_title
-#		self.url = url
+''' Stores the data for each class '''
+class ClassRatingData:
+	def __init__(self, class_name, class_rating):
+		self.class_name = class_name
+		self.class_rating = class_rating
 
 ''' Returns the page for a given url. Retries up to 10 times to retrieve the page. '''
 def get_page(url):
@@ -32,6 +30,87 @@ def get_page(url):
 			if max_tries == 0:
 				raise Exception('Connection aborted 10 times for ' + url)
 	return page
+
+def switch_statement(number):
+	return {
+		0: '1',
+		1: '3',
+		2: '5',
+		3: '8',
+		4: '10',
+		5: '12',
+		6: '15',
+		7: '17',
+		8: '19',
+		9: '22',
+	}[number]
+
+def get_class_rating(page, number):
+	tree = html.fromstring(page.content)
+	xPath = "/html/body/section/div/section/div[2]/div[2]/div[4]/div[" + switch_statement(number) + "]/div/div[1]/span/b//text()"
+	classRating = tree.xpath(xPath)
+	return classRating
+
+def get_class_list(url):
+	page = get_page(url)
+	html = page.text
+#	print html
+	classNameRegEx1 = '<div class="title circle main">(.+?)</div>'
+	classNamePattern1 = re.compile(classNameRegEx1)
+	classNamesAllClasses = re.findall(classNamePattern1, html)
+	class_rating_list = list()
+	i=0
+	j=0
+	while j<len(classNamesAllClasses):
+		class_rating_list.append(ClassRatingData(classNamesAllClasses[j], get_class_rating(page, i)))
+		j += 2
+		i += 1
+		if(i==10):
+			i=0
+	return class_rating_list
+
+''' writes the class ratings to csv files '''
+def write_to_csv(class_rating_list):
+	os_dir = os.path.dirname(__file__)
+	class_rating_file = os.path.join( \
+        os_dir, '../ClassSchedulizer/db/class_rating_data.csv')
+	class_rating_file = open(class_rating_file, 'w')
+	class_rating_writer = csv.writer(class_rating_file, delimiter=',', \
+        lineterminator='\r\n', quoting=csv.QUOTE_ALL)
+	class_rating_writer.writerow(( \
+        "class_name", "class_rating"))
+	for each_class in class_rating_list:
+		print "Writing data for " + each_class.class_name + " to file."
+		className = each_class.class_name
+		classRating = each_class.class_rating
+		class_rating_writer.writerow((each_class.class_name, each_class.class_rating))
+	class_rating_file.close()
+
+
+i=1
+while(i<=100):
+	url = "http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=" + str(i)
+	classRatings = get_class_list(url)
+	write_to_csv(classRatings)
+	i += 1
+
+#classRatings = get_class_list("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=1")
+#write_to_csv(classRatings)
+
+
+#	i =1
+######################################change back to 100
+#	while(i<=100):
+#		url = "http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=" + str(i)
+#		get_class_name_and_rating(url)
+#		i += 1
+
+
+
+
+
+
+
 
 # to test get_page()
 #get_page('http://www.bruinwalk.com')
@@ -59,36 +138,33 @@ def get_class_name_and_rating(url):
 			i=0
 
 
-def get_class_rating(page, number):
-	tree = html.fromstring(page.content)
-	xPath = "/html/body/section/div/section/div[2]/div[2]/div[4]/div[" + switch_statement(number) + "]/div/div[1]/span/b//text()"
-	classRating = tree.xpath(xPath)
-	return classRating
+
 
 #get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=1")
 #get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=2")
 #get_class_name_and_rating("http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=3")
 
-def switch_statement(number):
-	return {
-		0: '1',
-		1: '3',
-		2: '5',
-		3: '8',
-		4: '10',
-		5: '12',
-		6: '15',
-		7: '17',
-		8: '19',
-		9: '22',
-	}[number]
+
 
 
 def output_all_class_names():
 	i =1
+######################################change back to 100
 	while(i<=100):
 		url = "http://www.bruinwalk.com/search/?sort=alphabetical&category=classes&page=" + str(i)
 		get_class_name_and_rating(url)
 		i += 1
 
-output_all_class_names()
+
+
+
+
+
+
+
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+#output_all_class_names()
+
+# ratings = get_all_class_names_and_ratings()
+# write_to_csv(ratings)
