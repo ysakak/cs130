1df90f9b2ac20a8563de28cb4cd1f6f0ff811aee
Yuuma Sakakibara <yuuma.sakakibara94@gmail.com>
2016-11-27 21:57:35 -0800

    bug fix

--- ClassSchedulizer/db/schema.rb
+++ ClassSchedulizer/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122215247) do
+ActiveRecord::Schema.define(version: 20161128033419) do
+
+  create_table "bruinwalk_ratings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "class_data", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "title"
