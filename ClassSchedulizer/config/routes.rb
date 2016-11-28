f774638bf4a35ab3cad7845822883d381ec13748
Chul Hee Woo <chwl3@ucla.edu>
2016-11-27 21:19:22 -0800

    changed root route to calendar. Added few more complex UI test scenarios

--- ClassSchedulizer/config/routes.rb
+++ ClassSchedulizer/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#title'
+  root 'calendar#index'
 
   get '/calendar' => 'calendar#index'
   get '/search' => 'class_data#index'
--- ClassSchedulizer/spec/features/ui_test_spec.rb
+++ ClassSchedulizer/spec/features/ui_test_spec.rb
@@ -5,7 +5,7 @@ require "capybara/rspec"
 
 feature "homepage has a calendar" do
   scenario "calendar has an ID of calendar" do
-    visit "/calendar"
+    visit "/"
 
     calendar_elem = find_by_id("calendar")
     caleendar_class = calendar_elem[:class]
@@ -16,7 +16,7 @@ end
 
 feature "homepage has a searchbar" do 
 	scenario "searchbar has a placeholder" do
-		visit "/calendar"
+		visit "/"
 
 		searchbar_elem = find_by_id("search")
 		searchbar_ph = searchbar_elem[:placeholder]
@@ -27,7 +27,7 @@ end
 
 feature "search and click result and back" do
 	scenario "search computer science and click CS31" do
-		visit "/calendar"
+		visit "/"
 
 		searchbar_elem = find_by_id('search')
 		searchbar_elem.set("Computer Science")
@@ -55,7 +55,7 @@ end
 
 feature "search and add class to schedule" do
 	scenario "search mathematics and add 31A to schedule" do
-		visit "/calendar"
+		visit "/"
 
 		searchbar_elem = find_by_id('search')
 		searchbar_elem.set("Mathematics")
@@ -70,5 +70,104 @@ feature "search and add class to schedule" do
 		page.has_css?('div .fc-title')
 		first('div', class: "fc-title", text: "Differential and Integral Calculus")
 
+	end
+end
+
+feature "search by GE category" do
+	scenario "click search by GE and pick cateogries" do
+		visit "/"
+
+		find('a', text: "Search by GE Category").click
+
+		select "Foundations of Arts and Humanities", :from => "foundation-search"
+		select "Literary and Cultural Analysis", :from => "category-search"
+		find_by_id("ge-search-submit").click
+
+		find('div', class: 'card-block',  text: 'ART HIS 23')
+	end
+end
+
+feature "search by keyword" do
+	scenario "search by keyword and look for result" do
+		visit "/"
+
+		find('a', text: "Search by Keyword").click
+
+		searchbar_elem = find_by_id("keyword-search")
+		searchbar_elem.set("programming")
+
+		find_by_id("keyword-search-submit").click
+
+		first('div', class: 'card-block',  text: 'Programming Languages')
+	end
+end
+
+feature "search, add, then delete class from schedule" do
+	scenario "search, add, then delete. Check calendar" do
+		visit "/"
+
+		searchbar_elem = find_by_id('search')
+		searchbar_elem.set("Physics")
+		searchbar_elem.native.send_keys(:return)
+
+		find('div', class: 'card-block',  text: 'PHYSICS 1A').click
+		find('a', text: "Lectures").click
+		first(:id, "independent-class-btn").click
+		find('a', text: "Discussions").click
+		first(:id, "add-section-btn").click
+
+		page.has_css?('div .fc-title')
+		first('div', class: "fc-title", text: "Physics for Scientists and Engineers: Mechanics")		
+
+		find_by_id("delete-class-btn").click
+
+		!page.has_css?('div .fc-title')
+	end
+end
+
+feature "search, add, then clear calendar" do
+	scenario "search Management, add class, then clear and check calendar" do
+		visit "/"
+
+		searchbar_elem = find_by_id('search')
+		searchbar_elem.set("Physics")
+		searchbar_elem.native.send_keys(:return)
+
+		find('div', class: 'card-block',  text: 'PHYSICS 1A').click
+		find('a', text: "Lectures").click
+		first(:id, "independent-class-btn").click
+		find('a', text: "Discussions").click
+		first(:id, "add-section-btn").click
+
+		page.has_css?('div .fc-title')
+		first('div', class: "fc-title", text: "Physics for Scientists and Engineers: Mechanics")		
+
+		find('input', id: "clear-calendar").click
+
+		!page.has_css?('div .fc-title')
+	end
+end
+
+feature "check requisite conflict" do
+	scenario "Add CS31, then check that CS131 is conflicted" do
+		visit "/"
+
+		searchbar_elem = find_by_id('search')
+		searchbar_elem.set("Computer Science")
+		searchbar_elem.native.send_keys(:return)
+
+		find('div', class: 'card-block',  text: 'COM SCI 33').click
+		find('a', text: "Lectures").click
+		first(:id, "independent-class-btn").click
+		find('a', text: "Discussions").click
+		first(:id, "add-section-btn").click
+
+		page.has_css?('div .fc-title')
+		first('div', class: "fc-title", text: "Introduction to Computer Organization")
+
+		searchbar_elem.set("Computer Science")
+		searchbar_elem.native.send_keys(:return)
+
+		find('div', class: 'invalid-card',  text: 'COM SCI 131')
 	end
 end
